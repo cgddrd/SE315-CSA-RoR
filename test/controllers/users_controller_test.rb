7c98125a06aab1fff352af41edfc5ca25b9dc1a5
Connor Goddard <connorlukegoddard@gmail.com>
2014-12-03 16:05:52 +0000

    Added fix for HTTP Authentication error within Rails functional tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,27 +1,50 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
   setup do
     @user = users(:one)
     @user_details = user_details(:one)
     session[:user_id] = @user_details
   end
 
+  # CG - Added test "teardown" method.
   def teardown
-    # as we are re-initializing @post before every test
-    # setting it to nil here is not essential but I hope
-    # you understand how you can use the teardown method
-
     @user = nil
     @user_details = nil
     session[:user_id] = nil
-
   end
 
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
+  end
+
+  test "should get index json" do
+    # get(:index, { :format => 'json' })
+
+    @request.headers['Authorization'] = "Basic #{Base64.encode64("admin:taliesin")}"
+    @request.headers['Accept'] = Mime::JSON
+    @request.headers['Content-Type'] = Mime::JSON.to_s
+
+    get :index
+
+    assert_equal 'application/json', @response.content_type
+    assert_response :success
+
+    user = JSON.parse(@response.body)
+    assert_equal "Loftus", user['surname']
+
+    # puts @request.content_type
+    # puts @response.content_type
+    # puts @response.body
+
+    # get :index
+
+    # assert_response :success
+    # assert_not_nil assigns(:users)
+
   end
 
   test "should get new" do
